5953997ad21e3a1f7ba74c9c87a2c663ef34b0e2
Yuncheng <fabioyang781@gmail.com>
2023-02-05 16:11:08 +1100

    add shell bash

--- action_scripts/execute.sh
+++ action_scripts/execute.sh
@@ -8,7 +8,7 @@ if [ -z "$AUTH" ]; then
 fi
 
 if [ -z "$QUERY_FILE_PATH" ]; then
-  stackql exec "$QUERY" --auth="${AUTH}"
+  stackql exec "$QUERY" --auth="${AUTH}" --output="${OUTPUT}" 
 else
-  stackql exec -i "$QUERY_FILE_PATH" --auth="${AUTH}"  
+  stackql exec -i "$QUERY_FILE_PATH" --auth="${AUTH}" --output="'${OUTPUT}'" 
 fi
